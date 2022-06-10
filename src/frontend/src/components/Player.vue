9bb511c7a2342dd0addbb8322c6d0011db5495d6
JacobLinCool <jacoblincool@gmail.com>
2022-06-10 20:42:46 +0800

    feat: smoother continue-playing

--- src/frontend/src/components/Player.vue
+++ src/frontend/src/components/Player.vue
@@ -24,60 +24,115 @@ let anime = ref<Anime>();
 
 const player_vid = inject<Ref<string>>("player_vid");
 const player_ep = inject<Ref<string>>("player_ep");
+const prev_vid = ref("");
+const prev_ep = ref("");
 
 watch(props, () => {
-    if (video) {
-        video.dispose();
-        video = undefined;
-    }
+    const reuse = prev_vid.value === props.vid;
+    let preloaded = false;
+    last_update = 0;
+    anime.value = undefined;
 
     let elm: HTMLVideoElement | undefined;
-    if (wrapper) {
-        elm = document.createElement("video");
-        elm.classList.add("video-js", "w-full", "max-h-4/5", "min-h-3/5", "focus:outline-none");
-        wrapper.value?.appendChild(elm);
+    if (!reuse) {
+        video?.dispose();
+        video = undefined;
+
+        if (wrapper) {
+            elm = document.createElement("video");
+            elm.classList.add("video-js", "w-full", "max-h-4/5", "min-h-3/5", "focus:outline-none");
+            wrapper.value?.appendChild(elm);
+        }
+
+        console.log(`created video element`, elm);
     }
 
-    if (props.vid && props.ep && elm) {
-        video = videojs(elm, {
-            controls: true,
-            autoplay: true,
-            preload: "auto",
-            sources: [
-                {
-                    src: `${config.backend}/${props.vid}/${props.ep}/index.m3u8`,
-                    type: "application/x-mpegURL",
+    if (props.vid && props.ep) {
+        if (!video && elm) {
+            video = videojs(elm, {
+                controls: true,
+                autoplay: true,
+                preload: "auto",
+                sources: [
+                    {
+                        src: `${config.backend}/${props.vid}/${props.ep}/index.m3u8`,
+                        type: "application/x-mpegURL",
+                    },
+                ],
+                poster: `${config.backend}/store/cover?id=${props.vid}`,
+                userActions: {
+                    // @ts-ignore
+                    hotkeys(event: KeyboardEvent) {
+                        if (event.code === "Space") {
+                            video?.paused() ? video?.play() : video?.pause();
+                        } else if (event.code === "ArrowRight") {
+                            video?.currentTime(video?.currentTime() + 5);
+                        } else if (event.code === "ArrowLeft") {
+                            video?.currentTime(video?.currentTime() - 5);
+                        } else if (event.code === "ArrowUp") {
+                            video?.volume(video?.volume() + 0.1);
+                        } else if (event.code === "ArrowDown") {
+                            video?.volume(video?.volume() - 0.1);
+                        } else if (event.code === "KeyM") {
+                            video?.muted(!video?.muted());
+                        } else if (event.code === "KeyF") {
+                            video?.isFullscreen()
+                                ? video?.exitFullscreen()
+                                : video?.requestFullscreen();
+                        }
+                    },
                 },
-            ],
-            poster: `${config.backend}/store/cover?id=${props.vid}`,
-            userActions: {
-                // @ts-ignore
-                hotkeys(event: KeyboardEvent) {
-                    if (event.code === "Space") {
-                        video?.paused() ? video?.play() : video?.pause();
-                    } else if (event.code === "ArrowRight") {
-                        video?.currentTime(video?.currentTime() + 5);
-                    } else if (event.code === "ArrowLeft") {
-                        video?.currentTime(video?.currentTime() - 5);
-                    } else if (event.code === "ArrowUp") {
-                        video?.volume(video?.volume() + 0.1);
-                    } else if (event.code === "ArrowDown") {
-                        video?.volume(video?.volume() - 0.1);
-                    } else if (event.code === "KeyM") {
-                        video?.muted(!video?.muted());
-                    } else if (event.code === "KeyF") {
-                        video?.isFullscreen()
-                            ? video?.exitFullscreen()
-                            : video?.requestFullscreen();
+            });
+
+            elm.focus();
+
+            video.on("pause", () => {
+                video?.one("play", () => {
+                    video?.play();
+                });
+            });
+
+            video.on("enterpictureinpicture", () => {
+                emit("enter_pip");
+            });
+
+            video.on("leavepictureinpicture", () => {
+                emit("leave_pip");
+            });
+            video.on("timeupdate", () => {
+                update();
+                if (
+                    props.vid &&
+                    props.ep &&
+                    video &&
+                    video.readyState() >= 2 &&
+                    video.currentTime() > video.duration() - 300 &&
+                    preloaded === false
+                ) {
+                    preloaded = true;
+                    preload_next();
+                }
+            });
+
+            video.on("ended", () => {
+                if (anime.value) {
+                    const ani = anime.value;
+                    const eps = Object.values(ani.episodes);
+                    const idx = eps.findIndex((x) => x[1] === props.ep);
+
+                    if (idx >= 0 && idx < eps.length - 1) {
+                        if (player_vid && player_ep) {
+                            player_vid.value = eps[idx + 1][0];
+                            player_ep.value = eps[idx + 1][1];
+                        }
                     }
-                },
-            },
-        });
+                }
+            });
 
-        elm.focus();
-
-        last_update = 0;
-        anime.value = undefined;
+            console.log(`created video player`, video);
+        } else if (video) {
+            video.src(`${config.backend}/${props.vid}/${props.ep}/index.m3u8`);
+        }
 
         get(`${props.vid}/${props.ep}/status`)
             .then((res) => res.json())
@@ -95,65 +150,21 @@ watch(props, () => {
 
         store("info", { id: props.vid })
             .then((res) => res.json())
-            .then((data) => (anime.value = data));
+            .then((data) => {
+                anime.value = data;
+                console.log(`watching`, data);
+            });
 
         // @ts-ignore
-        video.spriteThumbnails({
+        video?.spriteThumbnails({
             url: `${config.backend}/${props.vid}/${props.ep}/sprite.jpg`,
             width: 192,
             height: 108,
         });
-
-        video.on("pause", () => {
-            video?.one("play", () => {
-                video?.play();
-            });
-        });
-
-        video.on("enterpictureinpicture", () => {
-            emit("enter_pip");
-        });
-
-        video.on("leavepictureinpicture", () => {
-            emit("leave_pip");
-        });
-
-        let preloaded = false;
-        video.on("timeupdate", () => {
-            if (
-                props.vid &&
-                props.ep &&
-                video &&
-                video.readyState() >= 2 &&
-                video.currentTime() > video.duration() - 300 &&
-                preloaded === false
-            ) {
-                preloaded = true;
-                preload_next();
-            }
-        });
-
-        video.on("ended", () => {
-            if (anime.value) {
-                const ani = anime.value;
-                const eps = Object.values(ani.episodes);
-                const idx = eps.findIndex((x) => x[1] === props.ep);
-
-                if (idx >= 0 && idx < eps.length - 1) {
-                    if (player_vid && player_ep) {
-                        player_vid.value = eps[idx + 1][0];
-                        player_ep.value = eps[idx + 1][1];
-
-                        if (video?.isFullscreen()) {
-                            setTimeout(() => {
-                                video?.requestFullscreen();
-                            }, 150);
-                        }
-                    }
-                }
-            }
-        });
     }
+
+    prev_vid.value = props.vid;
+    prev_ep.value = props.ep;
 });
 
 function close(payload: MouseEvent) {
@@ -200,8 +211,6 @@ async function preload_next() {
         }
     }
 }
-
-setInterval(update, 3000);
 </script>
 
 <template>
